f1d12e6b9dfbefd2ebf1d249a5922b8f4ad7c55b
Hugo Wang <hugowangchn@gmail.com>
2024-09-22 23:28:56 +1200

    redesign model and migrations

---
+++ app/Models/Catalog.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Catalog extends Model
+{
+    use HasFactory;
+
+    public function histories(): \Illuminate\Database\Eloquent\Relations\MorphMany
+    {
+        return $this->morphMany(History::class, 'historable');
+    }
+}
